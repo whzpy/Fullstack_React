331fad63f37c1434f9689a80e37caa4a1ab5915b
Hengzhi Wang <hengzhi.wang@gmail.com>
2025-01-05 23:53:46 -0500

    __vite config again

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-export default {
-  build: {
+export default defineConfig({
+  plugins: [react()],
+    build: {
     outDir: 'dist',
   },
-};
+})
