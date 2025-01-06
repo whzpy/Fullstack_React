08c0f52a809225f11ad84e970eb7bc7fa5f0b297
Hengzhi Wang <hengzhi.wang@gmail.com>
2025-01-06 00:11:25 -0500

    __added base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    build: {
+  base: '/eTech-test/',
+  build: {
     outDir: 'dist',
   },
 })
