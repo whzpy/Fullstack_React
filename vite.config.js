c20c7a77246d9c30993be9e07e1807701d7ae7f3
Hengzhi Wang <hengzhi.wang@gmail.com>
2025-01-12 21:13:50 -0500

    __new dir test

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/eTech-test/',
+  base: '/hengzhiapp/',
   build: {
     outDir: 'dist',
   },
