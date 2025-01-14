4f735d4afcf456e8df8c0536d53153a09c0e11e9
Hengzhi Wang <hengzhi.wang@gmail.com>
2025-01-13 21:32:45 -0500

    __News page with images

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,11 +5,14 @@ const Contact = () => {
   const [fname, setFname] = useState('')
   const [lname, setLname] = useState('')
   const [phone, setPhone] = useState('')
+  const [userId, setUserId] = useState(3)
   const [rows, setRows] = useState([])
-  const submitHandler = (e)=> {
+
+  const addHandler = (e)=> {
     e.preventDefault();
     if(fname && lname && phone){
-      let newRows = [...rows, [fname, lname, phone]]
+      setUserId(userId + 1);
+      let newRows = [...rows, [userId,fname, lname, phone]]
       setRows(newRows)
       setLname('')
       setFname('')
@@ -18,7 +21,7 @@ const Contact = () => {
   }
 
   return (
-    <div style = {{marginTop: '-300px', width: '100%'}}>
+    <div style = {{marginTop: '-100px', width: '100%'}}>
       <form style = {{ color:'blue', marginTop: '10px', marginBottom: '1px', width: '100%' }}>
         <div style = {{display: 'flex',justifyContent: 'space-between',  alignItems: "center",}}>
           <label style={{ flex: 1, marginRight: '5px', padding: '3px' }}> Fistname </label>
@@ -33,7 +36,7 @@ const Contact = () => {
           <input type="text" id = "phone" name="phone"  pattern="\d*" value={phone}
             onChange={(e)=> { if (!/^\d*$/.test(e.target.value)){alert("Wrong phone number")};
               setPhone(e.target.value)}} style={{ flex: 1, marginRight: '30px' }}/>
-          <Button type="submit" onClick={submitHandler}> Add a new record</Button>
+          <Button type="submit" onClick={addHandler}> Add a new record</Button>
         </div>      
       </form>
       <hr></hr>
@@ -41,32 +44,31 @@ const Contact = () => {
       <Table className="table table-striped" style = {{margin: '20px', width: '100%', border: '2px solid #ddd', textAlign: 'left' }}>
         <thead>
           <tr>
+            <th> UserID</th>
             <th> Fist Name</th>
             <th> Last Name</th>
             <th> Phone Number</th>
           </tr>
         </thead>
         <tbody>
-          <tr key = {20250111} >
+          <tr key = {1} >
+            <td>1</td>
             <td> Jackie11</td>
             <td> Wang</td>
             <td> 1122334455</td>
           </tr>
-          <tr key = {20250122} >
+          <tr key = {2} >
+            <td>2</td>
             <td> Jackie22</td>
             <td> Wang</td>
             <td> 2233445566</td>
           </tr>
-            <tr key = {20250133} >
-            <td> Jackie33</td>
-            <td> Wang</td>
-            <td> 3344556677</td>
-          </tr>
           {rows[0] && rows.map((row, index) => (
-          <tr key = {index} >
+          <tr key = {userId} >
             <td> {row[0]}</td>
             <td> {row[1]}</td>
             <td> {row[2]}</td>
+            <td> {row[3]}</td>
           </tr>
           ))}
         </tbody>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import sessionTokenImage from '../assets/session_Token.png';
+import JWTImage from '../assets/img_JWT.png';
+
 
 const News = () => {
-  return <div style = {{marginTop: '-100px', width: '100%'}}>
+  return (
+  <div style = {{marginTop: '-100px', width: '100%'}}>
     <h3>News _ Tokens for User Validation</h3>
-    <a rel="stylesheet" href="https://chat.deepseek.com/a/chat/s/f8a25b70-28a9-44f4-a6f6-cb40d5aff6e4" target="_blank">10+ Kinds of Tokens by Deepseek </a>
-    </div>;
+    <a rel="stylesheet" href="https://chat.deepseek.com/a/chat/s/f8a25b70-28a9-44f4-a6f6-cb40d5aff6e4" target="_blank">10+ Kinds of Tokens by Deepseek </a> 
+    <hr></hr>
+    <div>
+      <img src={sessionTokenImage} alt="Session Token img" width="600" />
+      <img src={JWTImage} alt="Session Token img" width="600" />
+    </div>
+    <hr></hr>
+    <div >
+      <p style={{ display: 'inline' }}>Session Token is a kind of UUID, </p> 
+      <a rel="online UUID generator" href="https://fusionauth.io/dev-tools/uuid-generator" style={{ display: 'inline' }} target="_blank"> UUID generator </a>
+      <a rel="JWT Decoder" href="https://jwt.io/" style={{ display: 'inline', marginLeft: '200px' }} target="_blank"> JWT Decoder </a>
+    </div>
+  </div>
+  )
 };
 
 export default News;
