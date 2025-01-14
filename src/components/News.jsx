ea2dd336ff5b47a128a6bd21babb52588f70447d
Hengzhi Wang <hengzhi.wang@gmail.com>
2025-01-13 19:24:01 -0500

    __updated News page

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const News = () => {
-  return <div><h2>News Page</h2></div>;
+  return <div style = {{marginTop: '-100px', width: '100%'}}>
+    <h3>News _ Tokens for User Validation</h3>
+    <a rel="stylesheet" href="https://chat.deepseek.com/a/chat/s/f8a25b70-28a9-44f4-a6f6-cb40d5aff6e4" target="_blank">10+ Kinds of Tokens by Deepseek </a>
+    </div>;
 };
 
 export default News;
